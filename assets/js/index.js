e7d6bd0f493fbf54f8fab94538e7f4bbcea6ab26
Sergio Azevedo <sergior.jr@gmail.com>
2020-05-09 15:09:28 +0200

    Fixes css and transformations

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
@@ -43,13 +43,16 @@
         } catch (e) {}
 
         // Fade-in main header background image:
-        $('.main-header').imagesLoaded( { background: true }, function() {
-            $('.main-header-background-square').css('background-color', 'transparent');
+        $('.main-header').imagesLoaded({
+            background: true
+        }, function () {
+            $('.main-header-background-square').css('background-color', 'black');
+            $('.main-header-background-square').css('opacity', '0.2');
         });
 
         // Show main header's title and description when background image is visible:
         $(".home-template .main-header-background-square")
-            .on("transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd", function(e) {
+            .on("transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd", function (e) {
                 $('.home-template .page-title, .home-template .page-description').css('display', 'inherit');
             });
 
@@ -59,22 +62,24 @@
         });
 
         // Fade-in logo when image is loaded:
-        $('.blog-logo img').imagesLoaded(function() {
+        $('.blog-logo img').imagesLoaded(function () {
             $('.blog-logo img').css('display', 'inherit');
         });
 
         // Remove animation from menu when visible for first time:
-        $('.nav-closed .menu-button').click(function() {
+        $('.nav-closed .menu-button').click(function () {
             $(this).css('animation', 'none');
         });
 
         // Show author image when image is loaded:
-        $('.author-image .img').imagesLoaded( { background: true }, function() {
+        $('.author-image .img').imagesLoaded({
+            background: true
+        }, function () {
             $('.author-image').css('visibility', 'visible');
         });
 
         // Open/close navigation menu with ESC key:
-        window.document.onkeydown = function(event) {
+        window.document.onkeydown = function (event) {
             if (event.repeat === false && event.code === 'Escape') {
                 $("body").toggleClass("nav-opened nav-closed");
             }
@@ -87,11 +92,11 @@
     $.fn.arctic_scroll = function (options) {
 
         var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+                elem: $(this),
+                speed: 500
+            },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
         allOptions.elem.click(function (event) {
             event.preventDefault();
@@ -103,15 +108,20 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top + toMove)
+                }, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: toMove
+                }, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top)
+                }, allOptions.speed);
             }
         });
 
     };
 })(jQuery);
-
